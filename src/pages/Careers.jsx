6cf8ce52647796d5e3bd3a5b403048c0f7ccd3a9
Rex <lubwamaemmanuel1@gmail.com>
2023-08-29 17:12:10 +0300

    refactor:(simple refactoring)

---
+++ src/pages/Careers.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+
+function Careers() {
+  return (
+    <div>
+      <Header />
+      <p>Careers</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Careers;
---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,16 @@
+import Cover from "../components/contactUs/Cover";
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+
+function ContactUs() {
+  return (
+    <div>
+      <Header />
+      <Cover />
+      <p>Contact us</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default ContactUs;
